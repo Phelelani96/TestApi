ea89e65ce135d49bbebd4aca2d34cbaf17e6859a
Phelelani Seyisi <phelelani.seyisi@batsamayi.com>
2023-04-10 22:55:37 +0200

    Phase 2 of the API

--- TestApi/Models/Student.cs
+++ TestApi/Models/Student.cs
@@ -1,9 +1,14 @@
-﻿namespace TestApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestApi.Models
 {
     public class Student
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
         public string FullName 
         {
@@ -12,6 +17,8 @@
                 return FirstName + " " + LastName ; 
             }
         }
+        [Required]
         public int Age { get; set; }
+        public ICollection<Enrollment>? enrollments { get; set; }
     }
 }
--- TestApi/Program.cs
+++ TestApi/Program.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json.Serialization;
 using TestApi.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TestApiContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("TestApiContext") ?? throw new InvalidOperationException("Connection string 'TestApiContext' not found.")));
-
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(op => op.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Client",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+}
+    );
 
 var app = builder.Build();
 
@@ -20,7 +31,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
